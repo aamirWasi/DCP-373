df115445434bd8eae790794c5c4fe36dcf54ef76
aamir <wasi3aamir@gmail.com>
2019-01-29 02:05:13 +0600

    TLE

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                long Num1 = long.Parse(space[0]);
+                if (Num1 > 0)
+                {
+                    Console.WriteLine("Case {0}: {1} is a valid result",t,Num1);
+                }
+                else
+                {
+                    Console.WriteLine("Case {0}: {1} is not a valid result", t, Num1);
+                }
+            }
+        }
+    }
+}
